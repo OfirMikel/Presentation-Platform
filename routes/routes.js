6f83deb9da9b4b3bfc14775c7575e5b98f657446
Ofir Mikel <159753ofir@gmail.com>
2024-08-07 17:46:43 +0300

    -main: fixed problems with merging to main from post

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import getAllPresentations from "./get/getAllPresentations.js";
 import addPresentation from "./post/postPresentation.js";
+import addSlide from "./post/postSlide.js";
 
 export default function routes (app){
     app.get('/presentations', async (req, res) => {
         await getAllPresentations(res);
     });
+
 
     app.post('/presentation', async (req, res) => {
         await addPresentation(req,res);
