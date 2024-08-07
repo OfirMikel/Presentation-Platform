bbf28aa382d20c2e1a6ee6953a224cac59bbdee1
Ofir Mikel <159753ofir@gmail.com>
2024-08-07 17:49:53 +0300

    -post: added add slide method

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,16 @@
 import getAllPresentations from "./get/getAllPresentations.js";
 import addPresentation from "./post/postPresentation.js";
 import addSlide from "./post/postSlide.js";
+import getByTitle from "./get/getByTitle.js";
 
 export default function routes (app){
     app.get('/presentations', async (req, res) => {
         await getAllPresentations(res);
     });
+
+    app.get('/presentation/:title', async (req, res) => {
+        await getByTitle(req,res);
+    })
 
     app.post('/presentation', async (req, res) => {
         await addPresentation(req,res);
