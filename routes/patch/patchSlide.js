f9b21a3b7d208fc6575cd0f247b1e6b631de5bb0
Ofir Mikel <159753ofir@gmail.com>
2024-08-07 20:40:52 +0300

    -post: updated the patch request for patch Slide and patch Authors

---
+++ routes/patch/patchSlide.js
@@ -0,0 +1,28 @@
+import Slide from "../../schemes/slide.js";
+
+export default async function alterSlide(req, res) {
+    let {presentation, content, style} = req.body;
+    const id = req.params.id;
+
+    if (!id) {
+        return res.status(400).send("id is required");
+    }
+
+    try {
+        let slideById  = await Slide.findOne({_id: id});
+        if (!slideById) {
+            return res.status(400).send("Presentation with this title does not exists");
+        }
+        await Slide.updateOne({_id: id}, {
+            $set: {
+                presentation: presentation || slideById.presentation,
+                content: content || slideById.content,
+                style: style || slideById.style,
+            },
+        })
+        console.log(`Updated the ${id} slide with the new information`);
+        res.status(201).send(`Updated the ${id} slide with the new information`);
+    } catch (err) {
+        res.status(400).json({error: err.message});
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,8 @@ import getAllPresentations from "./get/getAllPresentations.js";
 import addPresentation from "./post/postPresentation.js";
 import addSlide from "./post/postSlide.js";
 import getByTitle from "./get/getByTitle.js";
+import alterAuthors from "./patch/patchAuthors.js";
+import alterSlide from "./patch/patchSlide.js";
 
 export default function routes (app){
     app.get('/presentations', async (req, res) => {
@@ -18,5 +20,12 @@ export default function routes (app){
 
     app.post('/slide', async (req, res) => {
         await addSlide(req,res);
+    })
+    app.patch('/presentation/authors/:title', async (req, res) => {
+        await alterAuthors(req,res);
+    })
+
+    app.patch('/slide/:id', async (req, res) => {
+        await alterSlide(req,res);
     })
 }
