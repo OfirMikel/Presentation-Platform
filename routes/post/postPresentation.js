ce9555f1331eb84394cbb3df5560ac3a900cbfe2
Ofir Mikel <159753ofir@gmail.com>
2024-08-07 16:01:08 +0300

    -post: addPresentation | added the addPresentation feature and

--- routes/post/postPresentation.js
+++ routes/post/postPresentation.js
@@ -4,7 +4,7 @@ export default async function addPresentation(req , res) {
     try {
         const newPresentation = await Presentation.create(req.body);
         res.status(201).json(newPresentation);
-        console.log("added");
+        console.log("Added Presentation");
     } catch (err) {
         res.status(400).json({error: err.message});
     }
